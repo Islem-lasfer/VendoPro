7cfc8836e58606be326a689499ec70d7d110b9e5
unknown <islamsifou2002@gmail.com>
2026-02-16 13:51:06 +0100

    Remove all files from repository

--- license-server/config/generate-keys.sh
+++
@@ -1,5 +0,0 @@
-# Generate a 2048-bit RSA private key for license signing
-# Run this on your secure server, never share the private key
-openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
-# Extract the public key for embedding in the client
-openssl rsa -in private_key.pem -pubout -out public_key.pem
